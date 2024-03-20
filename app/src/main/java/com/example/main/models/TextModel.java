0170ef86a6b433b0ba0542a42eb337faee9948a8
Arsenii Strelnikov <arsss1980@gmail.com>
2024-03-20 09:16:55 +0400

    Добавил конструкторы, геттеры и сеттеры в классы TextModel, UserModel.

--- app/src/main/java/com/example/main/models/TextModel.java
+++ app/src/main/java/com/example/main/models/TextModel.java
@@ -7,4 +7,29 @@ public class TextModel
     private String content;
 
     // Конструктор, геттеры и сеттеры
+    public TextModel()
+    {
+        this.title = "";
+        this.content = "";
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getContent()
+    {
+        return this.content;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
 }
--- app/src/main/java/com/example/main/models/UserModel.java
+++ app/src/main/java/com/example/main/models/UserModel.java
@@ -7,4 +7,29 @@ public class UserModel
     private String email;
 
     // Конструктор, геттеры и сеттеры
+    public UserModel()
+    {
+        this.username = "";
+        this.email = "";
+    }
+
+    public String getUsername()
+    {
+        return this.username;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
 }
